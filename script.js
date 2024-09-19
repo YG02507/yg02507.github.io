ce598c680ab95266742c446883201efb5429c1cd
YG02507 <YG02507@users.noreply.github.com>
2024-09-20 07:24:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const modal = document.getElementById("modal")
+const buttonModal = document.getElementById("btn-modal")
 
 function modalOn() {
-    modal.style.display="flex"
+    modal.style.display = "flex"
 }
-function isModalOn() {
-    return modal.style.display === "flex"
-}
-function modalOff() {
-    modal.style.display = "none"
-}
-const btnModal = document.getElementById("btn-modal")
+// function modalOff() {
+//     modal.style.display = "none"
+// }
+// function isModalOn() {
+//     return modal.style.display === "flex"
+// }
 
-btnModal.addEventListener("click", e => modalOn())
+buttonModal.addEventListener("click", e => modalOn())
