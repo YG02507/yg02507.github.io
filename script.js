8d343324d44c40ac4538f90be2f6914381e1b250
YG02507 <YG02507@users.noreply.github.com>
2025-09-04 19:44:20 +0900

    yg02507.github.io [2025-09-04-19-41]
    yg02507.github.io [2025-09-04-19-41]

--- script.js
+++ script.js
@@ -7,19 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     tab.addEventListener('click', (e) => {
       e.preventDefault();
 
-      // 모든 탭 비활성화
-
       tabs.forEach(t => t.setAttribute('aria-selected', 'false'));
-      
-      // 클릭된 탭 활성화
 
       e.currentTarget.setAttribute('aria-selected', 'true');
       
-      // 모든 탭 내용 숨기기
-      
       tabContents.forEach(content => content.setAttribute('hidden', ''));
-      
-      // 클릭된 탭에 해당하는 내용만 보이기
 
       const targetId = e.currentTarget.getAttribute('href').substring(1);
 
